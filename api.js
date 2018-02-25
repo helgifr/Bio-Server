c31d4f2e44890cbb1519ae9ab22304edd304d42f
Ytreg <helgifreyr95@gmail.com>
2018-02-25 01:49:15 +0000

    nota token til að sækja gögn

--- api.js
+++ api.js
@@ -1,8 +1,8 @@
 const express = require('express');
-const xss = require('xss');
+// const xss = require('xss');
 
-const { check, validationResult } = require('express-validator/check');
-const { sanitize } = require('express-validator/filter');
+// const { check, validationResult } = require('express-validator/check');
+// const { sanitize } = require('express-validator/filter');
 
 const request = require('ajax-request');
 
@@ -27,23 +27,33 @@ function catchErrors(fn) {
 }
 
 async function getMovies(req, res, next) {
-  const { id } = req.params;
+  // const { id } = req.params;
+
+  const token = await request({
+    url: 'http://api.biomynd.is/authenticate',
+    type: 'POST',
+    data: {
+      username: 'username', // VANTAR
+      password: 'password', // VANTAR
+    },
+  }, (error, response, body) => ({ err: error, res: response, body }));
+  console.log('token:', token);
+  console.log('err:', token.err);
+  console.log('res:', token.res);
+  console.log('body:', token.body);
 
   const data = await request({
-    url : 'http://api.biomynd.is/authenticate',
-    type : 'POST',
-    data : {
-      username : "username", // VANTAR
-      password : "password" // VANTAR
-    }
-  }, (err, res, body) => {
-    return { err: err, res: res, body: body };
-  });
-  console.log(data);
+    url: 'http://api.biomynd.is/movies',
+    type: 'GET',
+    headers: {
+      'x-access-token': token,
+    },
+  }, (error, response, body) => ({ err: error, res: response, body }));
+  console.log('data:', data);
   console.log('err:', data.err);
   console.log('res:', data.res);
   console.log('body:', data.body);
-  
+
   return res.json(data.body);
 }
 
