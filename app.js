9222b17b398505d581dc4cadc0cf34bb80619af5
Helgi Freyr Helgason <helgi.helgason@comeon.com>
2019-09-19 15:46:33 +0200

    lagaði cors

--- app.js
+++ app.js
@@ -1,17 +1,13 @@
 require('dotenv').config();
 const express = require('express');
 const api = require('./api');
+const cors = require('cors');
 
 const app = express();
 
 app.use(express.json());
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-  next();
-});
+app.use(cors());
 
 app.use('/', api);
 
