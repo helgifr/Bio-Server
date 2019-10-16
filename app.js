0dbcbe2560c5007fd19c0d99862791d0fd8367b0
helgifr <helgifreyr95@gmail.com>
2019-10-16 15:56:46 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 require('dotenv').config();
 const express = require('express');
 const api = require('./api');
-const cors = require('cors');
 
 const app = express();
 
 app.use(express.json());
 
-app.use(cors());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  next();
+});
 
 app.use('/', api);
 
